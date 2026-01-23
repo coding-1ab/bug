4cbfb3753a23c724d77f6b9fa1a68c5478bd6273
dktsudgg <dktsudgg@yonsei.ac.kr>
2026-01-24 01:10:41 +0900

    fix(server): 클라이언트 패킷 수신 로직에서 유효성 검증 로직 사용, 패킷 파편화에 대응한 수신 처리 구현

--- src/server.rs
+++ src/server.rs
@@ -37,17 +37,10 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("detected new client. ({})", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
-                    stream.set_nonblocking(false)
-                        .expect("setting non blocking false fail..");
-                    stream.set_read_timeout(Some(Duration::from_secs(1)))
-                        .expect("setting read timeout fail..");
-
-                    handle_client(stream)
-                });
+                thread::spawn(move|| handle_client(stream));
             }
             Err(e) => {
-                println!("Error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
@@ -55,36 +48,71 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    // todo 지금은 최대 1024 바이트만 읽는데, 패킷 설계 나오면 제대로 크기만큼 읽자.
-    //
-    //  패킷 설계 예상. header, 게임 데이터 구조로...
-    //  | --- 고정 크기 바이트 Header --- | --- 게임 데이터 --- |
-    //  [ body byte length | ...    ]   [      body      ]
-    //
-    //  이렇게 고정 길이의 Header를 설계해서 Header 먼저 읽고,
-    //  그 안에 들어있는 byte length 필드를 읽어봐서 그만큼 더 읽자.
-    let mut data = [0 as u8; 1024];
+    let client_access_info = stream.peer_addr().unwrap();
 
-    while match stream.read(&mut data) {
-        Ok(0) => true,  // busy wait ... todo 어떻게 고치냐.
+    let mut buffer = Vec::with_capacity(2048);
+    let mut read_packet = [0u8; 1024];
+
+    'top_loop: while match stream.read(&mut read_packet) {
+        Ok(0) => {
+            println!("client disconnected. ({})", client_access_info);
+            false
+        },
         Ok(size) => {
-            // bytes to hex str
-            let hex_str = bytes_to_hex(&data[..size]);
-            println!("Received data = {} (from = {})", hex_str, stream.peer_addr().unwrap());
+            // 새로 들어온 패킷은 버퍼에 쌓아놓고, 로직은 버퍼를 사용한다.
+            // 이전에 들어온 패킷에서 남는 패킷이 존재하는 경우, 쌓아놓고 다음 분석에 사용되도록 함.
+            buffer.extend_from_slice(&read_packet[..size]);
 
-            // todo 클라이언트에 응답 전송하는 방법은 이렇게 하면 될듯.
-            // stream.write(text.as_bytes()).unwrap();
-            true
+            // bytes to hex str
+            let hex_str = bytes_to_hex(&buffer);
+            println!("received data = {} (from = {})", hex_str, client_access_info);
+
+            match network::validator::validate_packet_length(&buffer) {
+                Ok(remaining_byte_size) => {
+                    // actual: 1,2,3,4,5 / expected: 1,2,3 => remaining: 4,5 (2개)
+                    let message_bytes: Vec<u8> = buffer.drain(..buffer.len() - remaining_byte_size)
+                        .skip(2)
+                        .collect();
+                    println!("message bytes = {}", bytes_to_hex(&message_bytes));
+
+                    let result = network::MessageFromClient::new(&message_bytes);
+                    if let Err(e) = &result {
+                        eprintln!("{:?}", e);
+                        continue 'top_loop;
+                    }
+
+                    // todo ...
+                    let msg = result.unwrap();
+
+                    // todo 클라이언트에 응답 전송하는 방법은 이렇게 하면 될듯.
+                    //  stream.write(text.as_bytes()).unwrap();
+
+                    println!("{:?}", buffer);
+
+                    true
+                },
+                Err(short_msg_error @ network::error::NetworkError::ShortMsg{expected_length, actual_length}) => {
+                    // 패킷이 아직 부족한 경우에는 아무것도 하지 않음.
+                    println!("{}", short_msg_error);
+                    true
+                }
+                _ => {
+                    // todo 다른 오류 타입도 추가.
+                    false
+                }
+            }
         },
         Err(_) => {
-            println!("client disconnected. ({})", stream.peer_addr().unwrap());
-            stream.shutdown(Shutdown::Both).unwrap();
+            println!("client disconnected. ({})", client_access_info);
+            if let Err(e) = stream.shutdown(Shutdown::Both) {
+                eprintln!("failed to shutdown stream: {}", e);
+            }
             false
         }
     } {}
 }
 
-fn bytes_to_hex(bytes: &[u8]) -> String {
+fn bytes_to_hex(bytes: &Vec<u8>) -> String {
     bytes.iter()
         .map(|b| format!("{:02x}", b))
         .collect::<String>()
